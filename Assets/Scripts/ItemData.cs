f22fbb1f09f3229a373dad608cf4c1422ae07c23
Thaisond98 <147731298+dangthaison98@users.noreply.github.com>
2023-11-11 15:07:01 +0700

    Test

--- Assets/Scripts/ItemData.cs
+++ Assets/Scripts/ItemData.cs
@@ -1,18 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemData : MonoBehaviour
+[CreateAssetMenu]
+public class ItemData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[Flags]
+public enum ItemType
+{
+
 }
