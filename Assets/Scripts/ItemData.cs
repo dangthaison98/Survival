7a213ac54ba4a7311dc7ed0b49fde02466022ee6
Thaisond98 <147731298+dangthaison98@users.noreply.github.com>
2023-11-11 22:59:03 +0700

    Add URP

--- Assets/Scripts/ItemData.cs
+++ Assets/Scripts/ItemData.cs
@@ -1,3 +1,4 @@
+using Sirenix.OdinInspector;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,11 +7,25 @@ using UnityEngine;
 [CreateAssetMenu]
 public class ItemData : ScriptableObject
 {
-    
+    public string _name;
+    [MultiLineProperty]
+    public string _description;
+    [PreviewField(50, ObjectFieldAlignment.Center)]
+    public Sprite _icon;
+    public ItemType _itemType;
 }
 
 [Flags]
 public enum ItemType
 {
+    Resources = 1 << 0,
+    Food = 1 << 1,
+    Plantable = 1 << 2,
+    Hat = 1 << 3,
+    Dress = 1 << 4,
+    Armor = 1 << 5,
+    Bag = 1 << 6,
+    Tool = 1 << 7,
+    MeleeWeapon = 1 << 8,
 
 }
